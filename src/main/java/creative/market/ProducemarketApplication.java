4de1ed5471cb1719e6eb8d6c3e41fed999c63fc5
HO <love47024702@naver.com>
2022-09-25 14:09:56 +0900

    init

---
+++ src/main/java/creative/market/ProducemarketApplication.java
@@ -0,0 +1,13 @@
+package creative.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducemarketApplication.class, args);
+	}
+
+}
