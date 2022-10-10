1c0d3ac68eddc4852a51f4edde080f63d54d6110
gusals <gusals6586@gmail.com>
2022-10-10 22:07:43 +0900

    엔티티 수정

--- src/main/java/creative/market/domain/product/Product.java
+++ src/main/java/creative/market/domain/product/Product.java
@@ -4,6 +4,7 @@ import creative.market.domain.CreatedDate;
 import creative.market.domain.category.Kind;
 import creative.market.domain.category.KindGrade;
 import creative.market.domain.user.Seller;
+import creative.market.domain.user.User;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -36,17 +37,21 @@ public class Product extends CreatedDate {
     @JoinColumn(name = "kind_grade_id")
     private KindGrade kindGrade;
 
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @OneToMany(mappedBy = "product",cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductImage> productImages = new ArrayList<>();
 
     @Builder
-    public Product(String name, int price, String info, KindGrade kindGrade, Seller seller, List<ProductImage> ordinalProductImages , ProductImage signatureProductImage) {
+    public Product(String name, int price, String info, KindGrade kindGrade, User user, List<ProductImage> ordinalProductImages , ProductImage signatureProductImage) {
         this.name = name;
         this.price = price;
         this.info = info;
         this.kindGrade = kindGrade;
-        //Seller 연결해주기
+        this.user = user;
+
         ordinalProductImages.forEach(productImage -> productImage.changeProduct(this));
         signatureProductImage.changeProduct(this);
 
