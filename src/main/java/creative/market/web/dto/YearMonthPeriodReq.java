466a81f535c465c493ae375112452f225553cd0e
HO <love47024702@naver.com>
2022-11-20 00:04:27 +0900

    validation 메시지 공통화

--- src/main/java/creative/market/web/dto/YearMonthPeriodReq.java
+++ src/main/java/creative/market/web/dto/YearMonthPeriodReq.java
@@ -1,13 +1,11 @@
 package creative.market.web.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotNull;
 import java.time.YearMonth;
 
 @AllArgsConstructor
@@ -17,10 +15,8 @@ import java.time.YearMonth;
 public class YearMonthPeriodReq {
 
     @DateTimeFormat(pattern = "yyyy-MM")
-    @NotNull(message = "시작 날짜를 입력해주세요")
     YearMonth startDate;
 
     @DateTimeFormat(pattern = "yyyy-MM")
-    @NotNull(message = "종료 날짜를 입력해주세요")
     YearMonth endDate;
 }
