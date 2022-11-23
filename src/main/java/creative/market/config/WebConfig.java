8365f2d876021214d8d0709ba46b1bd53fb55f13
HO <love47024702@naver.com>
2022-11-24 01:04:22 +0900

    cors추가

--- src/main/java/creative/market/config/WebConfig.java
+++ src/main/java/creative/market/config/WebConfig.java
@@ -18,6 +18,18 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-       registry.addMapping("/**").allowedOrigins("http://localhost:3000","https://bkkang1.github.io:443","https://bkkang1.github.io");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000"
+                        ,"https://bkkang1.github.io:443"
+                        ,"https://bkkang1.github.io"
+                        ,"http://market-kumoh.shop"
+                        ,"112.217.167.202"
+                        ,"http://112.217.167.202"
+                        ,"https://112.217.167.202"
+                        ,"https://localhost:3000"
+                        ,"localhost:3000"
+
+                )
+                .allowCredentials(true);
     }
 }
